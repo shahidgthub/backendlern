8146451a6ee989234b370275d021eb3c78fc80e2
shahidgthub <azamshahid052@gmail.com>
2025-01-05 11:12:53 +0400

    add filter commit

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
-
 const express = require('express');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
+app.get("/api/products", (req, res) => {
+  const products = [
+    {
+      id: 1,
+      name: "tablewooden",
+      price: 300
+    },
+    {
+      id: 2,
+      name: "glass",
+      price: 100
+    },
+    {
+      id: 3,
+      name: "knife",
+      price: 500
+    },
+    {
+      id: 4,
+      name: "climb",
+      price: 50
+    },
+    {
+      id: 5,
+      name: "climb",
+      price: 50
+    }
+  ];
 
+  // Filter products based on the "search" query parameter
+  if (req.query.search) {
+    const search = req.query.search.toLowerCase(); // Convert search term to lowercase for case-insensitive comparison
+    const filteredProducts = products.filter(product =>
+      product.name.toLowerCase().includes(search)
+    );
+    res.send(filteredProducts);
+    return;
+  }
 
-app.get("/png", (req, res) => {
-  res.send('pong');
+  // Delay response by 3 seconds if no search query is provided
+  setTimeout(() => {
+    res.send(products);
+  }, 3000);
 });
-app.get("/twitter", (req, res) => {
-  res.send('<h1> this is twitter</h1>');
-});
-app.get("/yutube", (req, res) => {
-  res.send('<h1> this is youtube</h1>');
-});
- 
-app.listen(process.env.PORT, () => {
-  console.log(`This is setwiteerver side. Port ${PORT} is active`);
+
+app.listen(PORT, () => {
+  console.log(`Server is running. Port ${PORT} is active`);
 });
